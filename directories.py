fceab59f84e1740bfa926918b8aa3f6c5ef0daa1
Deboneil <bhattacharjeedeboneil@gmail.com>
2023-09-15 22:50:44 +0530

    nothing new

--- directories.py
+++ directories.py
@@ -3,4 +3,4 @@ from pathlib import Path
 path = Path("bootup.py")
 #path.mkdir()  #-- creates a directory named bootup
 
-#path.rmdir()  #-- removes the directory named bootups
+##path.rmdir()  #-- removes the directory named bootups
